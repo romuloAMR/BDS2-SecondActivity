3d22dcebcb7de78f7d9505cdace7363811349c8e
romuloAMR <amr.rmulo@gmail.com>
2025-01-04 11:02:17 -0300

    remove trash

--- rbtree/target/debug/deps/rbtree-3055fd1df131c2d6.d
+++
@@ -1,8 +0,0 @@
-/home/romulo/Codes/BDS2-SecondActivity/rbtree/target/debug/deps/librbtree-3055fd1df131c2d6.rmeta: src/lib.rs src/models/mod.rs src/models/rbtree.rs src/models/color.rs
-
-/home/romulo/Codes/BDS2-SecondActivity/rbtree/target/debug/deps/rbtree-3055fd1df131c2d6.d: src/lib.rs src/models/mod.rs src/models/rbtree.rs src/models/color.rs
-
-src/lib.rs:
-src/models/mod.rs:
-src/models/rbtree.rs:
-src/models/color.rs:
--- rbtree/target/debug/deps/rbtree-cbbd6517a326a3ac.d
+++
@@ -1,8 +0,0 @@
-/home/romulo/Codes/BDS2-SecondActivity/rbtree/target/debug/deps/librbtree-cbbd6517a326a3ac.rmeta: src/lib.rs src/models/mod.rs src/models/rbtree.rs src/models/color.rs
-
-/home/romulo/Codes/BDS2-SecondActivity/rbtree/target/debug/deps/rbtree-cbbd6517a326a3ac.d: src/lib.rs src/models/mod.rs src/models/rbtree.rs src/models/color.rs
-
-src/lib.rs:
-src/models/mod.rs:
-src/models/rbtree.rs:
-src/models/color.rs:
