3d603427edc89398348c8be5e2ce0318654a5ed9
Ryan Silvestre <129810642+ryann0097@users.noreply.github.com>
2025-01-06 20:52:26 -0300

    vector to tree convertion

--- BinTree/Implementations/BinTree.cpp
+++ BinTree/Implementations/BinTree.cpp
@@ -227,3 +227,36 @@ void BinTree::printLevelOrder() {
         }
     }
 }
+
+// heapify function
+void BinTree::heapify(std::vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+// vector to list binary tree conversion
+BinTree* BinTree::vectorToMaxHeap(std::vector<int>& arr) {
+    if (arr.empty()) {
+        return nullptr;
+    }
+
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    return new BinTree(arr);
+}
